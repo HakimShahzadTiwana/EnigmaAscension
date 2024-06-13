1b66a81b94f33f77df67e4f6fee12e0657002211
Faraz Majid <farazmajid56@gmail.com>
2024-06-13 11:21:36 +0500

    Fix PlayerNameTag Color Bug

--- Source/EnigmaAscension/Private/UI/EAPlayerTagWidget.cpp
+++ Source/EnigmaAscension/Private/UI/EAPlayerTagWidget.cpp
@@ -3,9 +3,16 @@
 
 #include "UI/EAPlayerTagWidget.h"
 
-void UEAPlayerTagWidget::SetPlayerTagProperties(FString Name, bool IsTeamA)
+// void UEAPlayerTagWidget::SetPlayerTagProperties(FString Name, bool IsTeamA)
+// {
+// 	PlayerName->SetText(FText::FromString(Name));
+// 	FColor color = IsTeamA ? FColor(255,0,0) : FColor(0,0,255);
+// 	PlayerName->SetColorAndOpacity(color);
+// }
+
+void UEAPlayerTagWidget::SetPlayerTagProperties_Implementation(const FString& Name, bool IsTeamA)
 {
 	PlayerName->SetText(FText::FromString(Name));
-	FColor color = IsTeamA ? FColor(0,0,255) : FColor(255,0,0);
+	FColor color = IsTeamA ? FColor(255,0,0) : FColor(0,0,255);
 	PlayerName->SetColorAndOpacity(color);
 }
